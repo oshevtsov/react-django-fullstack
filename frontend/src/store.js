0225b4cd1288f364c916ad5cbffa92b7808b2e89
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2022-05-12 17:41:10 +0200

    Get rid of zustand

--- frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import create from "zustand";
-
-const useStore = create((set) => ({
-  currentUser: null,
-  setCurrentUser: (user) => set({ currentUser: user }),
-  unsetCurrentUser: () => set({ currentUser: null }),
-}));
-
-export default useStore;
