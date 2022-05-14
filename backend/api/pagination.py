c8ad16419200722d4318e1f6ae1df85cf3b4db3f
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2022-05-14 17:49:33 +0200

    Revert "Include page size in list view"
    This reverts commit 32d12170d0c7ce00e47d3be9a1b88d32f81d5f5b.

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -9,7 +9,6 @@ class APIPagination(pagination.PageNumberPagination):
     def get_paginated_response(self, data):
         return response.Response(
             OrderedDict([('page', self.page.number),
-                         ('page_size', self.page_size),
                          ('num_pages', self.page.paginator.num_pages),
                          ('next', self.get_next_link()),
                          ('previous', self.get_previous_link()),
