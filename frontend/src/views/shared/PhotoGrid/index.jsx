e451b4e6ff7778a6a32be09738cd8f9427619503
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2022-05-14 18:10:24 +0200

    Use search params for page navigation

--- frontend/src/views/shared/PhotoGrid/index.jsx
+++ frontend/src/views/shared/PhotoGrid/index.jsx
@@ -1,4 +1,4 @@
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, useSearchParams } from "react-router-dom";
 import { useEffect, useState, useCallback } from "react";
 import { useAuth } from "../../../auth";
 import { makeAuthorizedRequest, makeUnauthorizedRequest } from "../../../api";
@@ -10,15 +10,16 @@ const PhotoGrid = ({ apiURL, requireAuth }) => {
   const { logOut } = useAuth();
   const location = useLocation();
   const navigate = useNavigate();
-  const [page, setPage] = useState(1);
+  const [searchParams, setSearchParams] = useSearchParams();
   const [pageData, setPageData] = useState(null);
 
   const makeFetchURL = useCallback(() => {
+    const page = searchParams.get("page") ?? 1;
     const url = new URL(apiURL);
-    const searchParams = new URLSearchParams({ page });
-    url.search = searchParams.toString();
+    const urlSearchParams = new URLSearchParams({ page });
+    url.search = urlSearchParams.toString();
     return url;
-  }, [page, apiURL]);
+  }, [searchParams, apiURL]);
 
   const fetchData = useCallback(async () => {
     const url = makeFetchURL();
@@ -42,9 +43,16 @@ const PhotoGrid = ({ apiURL, requireAuth }) => {
       .catch(console.error);
   }, [fetchData, location.pathname, logOut, navigate, requireAuth]);
 
-  const controlsLegend = `${page} of ${pageData?.num_pages ?? page}`;
-  const handleClickPrev = pageData?.previous && (() => setPage(page - 1));
-  const handleClickNext = pageData?.next && (() => setPage(page + 1));
+  const page = pageData?.page;
+  const num_pages = pageData?.num_pages;
+  const controlsLegend = `${page} of ${num_pages}`;
+
+  const handleClickPrev =
+    page > 1 ? () => setSearchParams({ page: page - 1 }) : null;
+
+  const handleClickNext =
+    page < num_pages ? () => setSearchParams({ page: page + 1 }) : null;
+
   return pageData ? (
     <main className={styles.container}>
       <h1>Profile</h1>
